f747d811274b8163c81eff3cb793b15106ca0a64
Nguyen Duc Nghia <nguyenducnghia1607@gmail.com>
2023-12-11 09:54:05 +0700

    End of part 4 authentication-basics

---
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,21 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public static class ApplicationServicesExtension
+{
+    public static IServiceCollection AddApplicationServicers(this IServiceCollection service, IConfiguration config)
+    {
+        service.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+});
+        service.AddCors();
+
+        service.AddScoped<ITokenService, TokenService>();
+        return service;
+    }
+}
